b1d7a802ab91ad950e19b6ec646ad9e1e8d09e02
Jacob Vilevac <jakevilevac@gmail.com>
2023-03-09 21:33:59 -0800

    added breakdown component to show all data combined

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Helmet from './components/helmet.js'
+import Breakdown from './components/breakdown.js'
 import './App.css';
 
 function App() {
@@ -15,9 +16,7 @@ function App() {
       {/* Create a Helmet() component for each helmet mod */}
       <div id="helmet-mods">
         <Helmet modCount={helmetModCount} setModCount={setHelmetModCount}/>
-        <h3>Super Energy on Kill</h3>
-        <p>Melee Kill: %{((helmetModCount.handsOn.stacks[helmetModCount.handsOn.count]) * 100).toFixed(2)}</p>
-        <p>Nade Kill: %{((helmetModCount.ashesToAssets.stacks[helmetModCount.ashesToAssets.count]) * 100).toFixed(2)}</p>
+        <Breakdown helmetMods={helmetModCount}/>
       </div>
     </div>
   );
--- src/components/breakdown.js
+++ src/components/breakdown.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './css/breakdown.css';
 
 function Breakdown(props) {
-
+    const { helmetMods } = props;
+    console.log(helmetMods)
+    return (
+        <div className='breakdown'>
+            <div>
+                <h4>Super Energy</h4>
+            </div>
+        </div>
+    )
 }
 
 export default Breakdown;
