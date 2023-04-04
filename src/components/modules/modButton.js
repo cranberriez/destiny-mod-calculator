c00b0db4d259233627d196b52689672f42399428
Jacob Vilevac <jakevilevac@gmail.com>
2023-04-04 06:48:33 -0700

    added setTimeout to modbutton hover to look better on mobile

--- src/components/modules/modButton.js
+++ src/components/modules/modButton.js
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 function ModButton(props) {
     const { i, slotted, setSlotted, setModCount, url, generates, modID} = props;
+    const [hovered, setHovered] = useState(false);
+    let timer = null;
+
+    function handleMouseEnter() {
+        clearTimeout(timer);
+        setHovered(true);
+        timer = setTimeout(() => {
+            setHovered(false);
+        }, 1000);
+    }
+
+    function handleMouseLeave() {
+        setHovered(false);
+        clearTimeout(timer);
+    }
+
+    useEffect(() => {
+        return () => {
+            if (timer) {
+                clearTimeout(timer);
+            }
+        };
+    }, [timer]);
 
     function placeMod(countChange) {
         setModCount(countChange);
         const newSlotted = [...slotted]; // Create a new copy of the array
         if (countChange > 0) {
+            handleMouseEnter()
             newSlotted[i] = modID;
         } else {
             newSlotted[i] = '';
         }
         setSlotted(newSlotted);
-
     }
 
 
@@ -28,7 +51,9 @@ function ModButton(props) {
         return (
             <button
                 onClick={() => placeMod(1)}
-                className={slotStatus}
+                className={`${slotStatus} ${hovered ? 'hover' : ''}`}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
             >
                 {<FontAwesomeIcon icon={faPlus} />}
             </button>
@@ -38,7 +63,9 @@ function ModButton(props) {
         return (
             <button
                 onClick={() => placeMod(-1)}
-                className={slotStatus + ' ' + generates}
+                className={`${slotStatus} ${generates} ${hovered ? 'hover' : ''}`}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
             >
                 <img src={url} alt='Mod'></img>
                 {<FontAwesomeIcon icon={faPlus} />}
